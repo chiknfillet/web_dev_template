4d2d0007e1725b92b43ea2bffaa9e71dfbba276d
marlex <marlexestores241@gmail.com>
2025-08-29 03:18:11 +0800

    Added linting and prettier

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 Welcome to my basic web dev template
 
 Instructions:
+
 1. Uncomment the items in the .gitignore file
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import "./reset.css"
-import "./styles.css"
+import "./reset.css";
+import "./styles.css";
 
-console.log("Hello, World")
+console.log("Hello, World");
